f04341fc054401c8769a1998899f4343637f2a16
ivan.heibi <ivanhb.ita@gmail.com>
2022-12-06 16:47:47 +0100

    conf

---
+++ js/getconf.js
@@ -0,0 +1,22 @@
+function apply_conf(isindex = false) {
+  var base = "../";
+  if (isindex) {
+    base = "";
+  }
+  $.get(base+"conf.ini", function(data){
+    var lines = data.split("\n");
+    var index = {};
+    for (var i = 0; i < lines.length; i++) {
+      var line = lines[i].trim();
+      if ((line[0] == "#") || (line == "")){
+        continue;
+      }else {
+        var parts = line.split("=");
+        index[parts[0]] = parts[1];
+      }
+    }
+    var nav_tag = document.getElementsByTagName('nav')[0];
+    console.log(index);
+    nav_tag.innerHTML = nav_tag.innerHTML.replace(/__baseurl__/g,index["baseurl"]);
+  });
+}
